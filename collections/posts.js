9bf5eb71f4cdf080a27abe841ea8c776a27b75ae
Justin Schiff <AmbientRevolution@gmail.com>
2015-05-31 01:04:42 -0700

    post validation on server now.

--- collections/posts.js
+++ collections/posts.js
@@ -5,3 +5,27 @@ Posts.allow({
         return !!userId;
     }
 });
+
+Meteor.methods({
+    post: function (postAttributes) {
+        var user = Meteor.user();
+        var postWithSameLink = Posts.findOne({url: postAttributes.url});
+
+        if (!user) throw new Meteor.Error(401, "You must be logged in.");
+        if (!postAttributes.title) throw new Meteor.Error(422, "Please fill in a headline.");
+        if (postAttributes.url && postWithSameLink) {
+            throw new Meteor.Error(302,
+                "This link has already been posted.",
+                postWithSameLink._id);
+        }
+
+        var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), {
+            userId: user._id,
+            author: user.username,
+            submitted: new Date().getTime()
+        });
+
+        var postId = Posts.insert(post);
+        return postId;
+    }
+});
